e3aac788de2e0b5100a31e8f52a5eda7018c8395
SimoneFranzoni <simofranzoni99@gmail.com>
2021-11-02 16:54:27 +0100

    trying-js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
-function init(tot){
-    for(let i = 0; i < 64; i++){
+let cellnum;
 
-        // creo l'elemento square e lo aggiungo al container
-        const sq = createSquare(container);
-    
-        sq.addEventListener('click',function(){
-          //  console.log(this);
-            this.classList.add('clicked');
-        })
-    
-    }
-    
+if (difficult.value == "easy"){
+    cellnum = 100;
+} else if (difficult.value == "normal"){
+    cellnum = 81;
+} else if (difficult.value == "hard"){
+    cellnum = 49;
+}
+
+console.log(cellnum);
+
+for (let i = 0; i < 100; i++){
+    const sq = createSquare(container);
+    sq.innerHTML = i + 1;
+    sq.addEventListener('click',function(){
+        this.classList.add('clicked');
+    })
+}
+
+/**
+ * Generatore di square
+ * @param {HTMLDivElement} target 
+ * @returns 
+ */
+
+function createSquare(target){
+    const sq = document.createElement('div');
+    sq.classList.add('square');
+    target.append(sq);
+    return sq;
 }
